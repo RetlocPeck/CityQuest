582962f8cdf82bf76debbf04289d82261f7767a3
Harivansh Luchmun <hluchmun108@gmail.com>
2025-02-15 17:32:08 -0600

    Changed profile page template

--- frontend/CityQuest/src/pages/Profile.tsx
+++ frontend/CityQuest/src/pages/Profile.tsx
@@ -1,34 +1,57 @@
-import { IonButton, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import '.././stylesheets/Home.css';
+import { IonButton, IonCard, IonCol, IonContent, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import '.././stylesheets/Profile.css';
+import { useEffect, useState } from 'react';
 
 const Profile: React.FC = () => {
+
+    const [error, setError] = useState(""); //Stores the error message if there is one
+    const [visitationStatuses, setVisitationStatuses] = useState([]); //Stores the current vistation statuses
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+        try{
+            const results = await fetch("url", {}); // Fetch posts data from the external function
+        } catch (e) {
+            console.error(e);
+            setError("Error loading achievements list");
+        };
+        };
+        fetchUserProfile();
+      }, []);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-      {/**
-       * 
-       * Map will be on this screen
-       * 
-       * Add a toolbar at bottom that contains buttons for:
-       * - Acccount, Achievments
-       */}
-       <IonCard>
-        <IonToolbar>
-          <IonButton>Account</IonButton>
-          <IonButton>Achievments</IonButton>
-        </IonToolbar>
+    <IonContent fullscreen className="signup-bg">
+        <IonCard>
+            <IonRow>
+                <IonToolbar>
+                <IonButton >Back</IonButton>
+                    <IonButton slot='end'>Settings</IonButton>
+                </IonToolbar>
+            </IonRow>
+            
+            <IonRow>
+                <IonCol size="3" className="profile-picture">
+                    <img src="path_to_profile_picture" alt="Profile" />
+                </IonCol>
+                <IonCol size="9" className="username">
+                    <h2>Username</h2>
+                </IonCol>
+            </IonRow>
         </IonCard>
-      </IonContent>
+        <IonCard className="main-card">
+            
+            {visitationStatuses.map((status, index) => (
+                <IonRow key={index}>
+                    <IonCol size="12">
+                        <p>{status}</p>
+                    </IonCol>
+                </IonRow>
+            ))}
+
+        </IonCard>
+        
+    </IonContent>
     </IonPage>
   );
 };
