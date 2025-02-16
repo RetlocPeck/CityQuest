d84d894b17a14539f9886b24da9209cedc283c08
Salsasp <dakotagee99@gmail.com>
2025-02-15 19:14:52 -0600

    FIXED THIS BULLSHIgit add src/pages/SignUp.tsx git add src/pages/SignUp.tsx

--- frontend/CityQuest/src/pages/SignUp.tsx
+++ frontend/CityQuest/src/pages/SignUp.tsx
@@ -1,26 +1,26 @@
 import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    IonButton,
-    IonCardContent,
-    IonCard,
-    IonCardHeader,
-    IonRow,
-    IonCol,
-    IonCardTitle,
-  } from "@ionic/react";
-  import ExploreContainer from "../components/ExploreContainer";
-  import { useEffect, useState } from "react";
-  import "../stylesheets/SignUp.css";
-  import { getFirestore, doc, setDoc } from "firebase/firestore";
-  import { signUp } from '../services/authService';
-  import { app, analytics, auth, firestore, storage } from '../firebase-config';
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonCardContent,
+  IonCard,
+  IonCardHeader,
+  IonRow,
+  IonCol,
+  IonCardTitle,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { useEffect, useState } from "react";
+import "../stylesheets/SignUp.css";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { signUp } from '../services/authService';
+import { app, analytics, auth, firestore, storage } from '../firebase-config';
 
-  import star from "./sparkles.gif";
-  const SignUp: React.FC = () => {
+import star from "./sparkles.gif";
+const SignUp: React.FC = () => {
 
 
   const validate = () => {};
@@ -33,6 +33,28 @@ import {
     city: "",
   });
 
+  const handleSignup = async () => {
+      var user;
+      try {
+        const userCredential = await signUp(formData.email, formData.password);
+        console.log("Signed up successfully!", userCredential.user);
+        user = userCredential.user;
+        // Save additional user data to Firestore
+        await setDoc(doc(firestore, "users", user.uid), {
+          email: user.email,
+          createdAt: new Date(),
+          displayName: formData.firstName + " " + formData.lastSecond,
+          pinnedCities: [formData.city],
+        });
+        //setDoc()
+      } catch (error) {
+        console.error("Signup error:", error);
+        // Optionally, show an error message to the user
+      }
+
+      
+    };
+
   const sendUserData = async () => {
     const response = await fetch("url/signup", {
       method: "POST",
@@ -41,50 +63,6 @@ import {
       },
       body: JSON.stringify(formData),
     });
-
-    const handleSignup = async () => {
-        var user;
-        try {
-          const userCredential = await signUp(formData.email, formData.password);
-          console.log("Signed up successfully!", userCredential.user);
-          user = userCredential.user;
-          // Save additional user data to Firestore
-          await setDoc(doc(firestore, "users", user.uid), {
-            email: user.email,
-            createdAt: new Date(),
-            displayName: formData.firstName + " " + formData.lastSecond,
-            pinnedCities: [formData.city],
-          });
-          //setDoc()
-        } catch (error) {
-          console.error("Signup error:", error);
-          // Optionally, show an error message to the user
-        }
-
-        
-      };
-  
-    const sendUserData = async () => {
-      const response = await fetch("url/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        console.log("signup successful");
-      } else {
-        console.log("signup failed");
-      }
-    };
-  
-    return (
-      <IonPage>
-        <IonContent fullscreen className="signup-bg">
-        <div className="signup-center-con">
-        <img src= {star} alt="Star animation" style = {{width: "200px"}} />
 
     if (response.ok) {
       console.log("signup successful");
@@ -115,21 +93,63 @@ import {
                     setFormData({ ...formData, firstName: e.target.value })
                   }
                 />
-                </div>
-                </IonCardContent>
-                <IonButton
-                  className="signup-hover-solid wide"
-                  fill="clear"
-                  onClick={handleSignup}
-                >
-                  Submit
-                </IonButton>
-                </IonCard>
-                </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default SignUp;
-  
+      
+              <input className="signup-textboxes"
+                placeholder="Last Name"
+                type="text"
+                value={formData.lastSecond}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastSecond: e.target.value })
+                }
+              />
+              <input className="signup-textboxes"
+                placeholder="Email"
+                type="text"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+              <input className="signup-textboxes"
+                placeholder="Password"
+                type="password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
+              </div>
+              </IonCardContent>
+              </IonCard>
+              <IonCard className="signup-card2">
+              <IonCardHeader className = "signup-citybox">
+                  Enter your current city below
+              </IonCardHeader>
+                  <IonCardContent className="signup-topalign">
+                  <div className="signup-buttons">
+                  <input className="signup-textboxes"
+                placeholder="City"
+                type="text"
+                value={formData.city}
+                onChange={(e) =>
+                  setFormData({ ...formData, city: e.target.value })
+                }
+                
+              />
+              </div>
+              </IonCardContent>
+              <IonButton
+                className="signup-hover-solid wide"
+                fill="clear"
+                onClick={handleSignup}
+              >
+                Submit
+              </IonButton>
+              </IonCard>
+              </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SignUp;
